a21be52004ede9117e16812bd4a410634ce470d1
ejgavin <ejgavin11@gmail.com>
2025-05-29 23:26:23 -0400

    Initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@ import withBundleAnalyzer from '@next/bundle-analyzer';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: '/W/music',
   reactStrictMode: true,
   swcMinify: true,
   
